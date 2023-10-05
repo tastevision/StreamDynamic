29aa3c7e77537b57de42bb2c9108fe15179c085c
tasteSoGood <970879461@qq.com>
2023-10-05 10:27:50 +0800

    add exps/data

--- update_project.sh
+++ update_project.sh
@@ -1,2 +1,2 @@
 root=~/project/StreamDynamic
-rsync -avzz $root yb-s11:/home/xiang.huang/project/ --exclude-from=$root/exclude.txt
+rsync -avzz $root lab_pc:/home/xiang.huang/project/ --exclude-from=$root/exclude.txt
